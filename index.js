a1ff99c8a9002e9160bbdfcae15e6dc130f5f178
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-03-30 14:23:42 -0400

    yasiris changes

--- index.js
+++ index.js
@@ -19,41 +19,84 @@ let quotes = [
   
     // Part 1
     function changeTitle() {
-  
+      document.title = "Yasiris"
+
     }
-  
+    changeTitle()
   
     // Part 2
     function changeBodyBgColor() {
+      document.querySelector("body").style.background = "pink"
   
+
     }
+    changeBodyBgColor()
   
   
     // Part 3
     function removeLastFavoriteThing() {
-  
+      document.getElementById("favorite-things").lastElementChild.remove()
+
     }
-  
+    removeLastFavoriteThing()
   
     // Part 4
     function makeSpecialTitlesBigger() {
-  
+      //change the font size of the special titles 
+      document.querySelectorAll(".special-title").forEach((title) => { 
+        title.style.fontSize = "50px"
+
+      })
+     
+      
     }
-  
+    makeSpecialTitlesBigger()
   
     // Part 5
     function RemoveChicagoRace() {
-  
+      document.querySelectorAll("#past-races li").forEach((race)=> {
+        // if (race.innerHTML === "Chicago"){
+          // race.remove()
+          //console.log(race)
+        //} 
+        race.innerHTML === "Chicago" ? race.remove() : null
+      })
     } 
+    RemoveChicagoRace()
   
     // Bonus
     // Part 6 
     function addPastRace() {
+      //first: create element
+      //second: append element to the list
+      let Cuba = document.createElement("li")
+      Cuba.innerHTML = "Cuba"
+      document.querySelector("#past-races").appendChild(Cuba)
   
     }
-   
+   addPastRace()
     // Part 7
     function createNewBlogPost() {
+      let div = document.createElement("div")
+      div.setAttribute("class ","blog-post purple")
+      document.querySelector(".main").appendChild(div)
+
+      let h1 = document.createElement("h1")
+      h1.innerHTML = "MARIA LA O"
+      document.querySelector(".main").lastElementChild.appendChild(h1)
+      
+      let p = document.createElement("p")
+      p1.innerHTML = "MARIA LA A, MARIA LA O TU MAI ES P Y LA MIA NO"
+      document.querySelector(".main").lastElementChild.appendChild(p)
 
     }
+    createNewBlogPost()
 })
+
+
+
+
+{/* <div class="blog-post purple"> */}
+{/* <h1>Tokyo</h1> */}
+{/* <p>I DRIFTED MY CAR AROUND A PARKING GARAGE!</p> */}
+// </div>
